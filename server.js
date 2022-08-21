d2058317fc1f16cc52e41b32cb0fb5d3fec839a8
Oscar Guardian <geoffroy.guillotte@gmail.com>
2022-08-21 21:23:28 +1000

    changed cors order

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ require('dotenv').config({path: './config/.env'})
 connectDB()
 
 //Middleware
-app.use(cors())
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors())
 
 app.use('/', homeRoutes)
 app.use('/tasks', taskRouters)
